d9d80f780de0f980b222e913e69ea3ed5110c3ff
m2lovric <m2lovric@gmail.com>
2024-04-23 17:55:24 +0200

    Add project files.

---
+++ UnitTest/HomeControllerTests.cs
@@ -0,0 +1,34 @@
+using aspnet_core_unit_1.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace UnitTest
+{
+    public class HomeControllerTests
+    {
+        private Mock<ILogger<HomeController>> _logger = new Mock<ILogger<HomeController>>();
+
+        [Fact]
+        public void CheckCountValueTest_2_ViewResult()
+        {
+            var controller = new HomeController(_logger.Object);
+            int value = 2;
+
+            var result = controller.CheckCountValue(value);
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void CheckCountValueTest_500_Exception()
+        {
+            var controller = new HomeController(_logger.Object);
+            int value = 500;
+
+            var result = controller.CheckCountValue(value);
+            
+            Assert.Throws<Exception>(() => result);
+        }
+    }
+}
